3fe2df50fe889c76e2598c8e458438472004bb98
Edward Wang <wang.huiyang@outlook.com>
2024-05-22 10:27:32 +0800

    feat: pre-commit formatter

--- src/pages/First.tsx
+++ src/pages/First.tsx
@@ -1,10 +1,23 @@
 import { GlobalStyle } from '@/global/style'
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
+import Animated, { useSharedValue } from 'react-native-reanimated'
 
 const First = () => {
+  const width = useSharedValue(100)
+
   return (
     <View style={GlobalStyle.page}>
-      <Text>First Page</Text>
+      <Animated.View style={{
+          width,
+          height: 100,
+          backgroundColor: 'violet',
+        }}/>
+      <Button
+        onPress={() => {
+          width.value += 10
+        }}
+        title="press me"
+      />
     </View>
   )
 }
