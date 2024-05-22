13831ba259cbc420d58b822e3543fc8623825ec3
Edward Wang <wang.huiyang@outlook.com>
2024-05-22 17:31:16 +0800

    feat: new animation

--- App.tsx
+++ App.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from '@/pages/Home'
-import First from '@/pages/First'
+import { PageConfig } from '@/pages/config'
+
 
 const Stack = createNativeStackNavigator()
 
@@ -10,8 +11,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="First" component={First} />
+      <Stack.Screen name='Home' component={Home} />
+        {Object.keys(PageConfig).map(key => (
+          <Stack.Screen key={key} name={key} component={PageConfig[key]} />
+        ))}
       </Stack.Navigator>
     </NavigationContainer>
   )
