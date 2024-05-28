faef18a09552ae6442fef8cf54831b903d5a0dab
Edward Wang <wang.huiyang@outlook.com>
2024-05-28 18:44:22 +0800

    feat: layout animation  Changes to be committed: 	new file:   src/pages/Layout.tsx         modified:   src/pages/config.ts

--- src/pages/config.ts
+++ src/pages/config.ts
@@ -3,6 +3,7 @@ import CountDown from './CountDown'
 import Draggable from './Draggable'
 import Drop from './Drop'
 import FadeInText from './FadeInText'
+import Layout from './Layout'
 import Shake from './Shake'
 import TranslateX from './TranslateX'
 import Width from './Width'
@@ -16,4 +17,5 @@ export const PageConfig = {
   Drop: Drop,
   FadeInText: FadeInText,
   CountDown: CountDown,
+  Layout: Layout,
 }
